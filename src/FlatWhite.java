1646b565c3b91d7f7c764af5cb65fa0aaf45287c
alexrmr08 <walter.romero123.2@gmail.com>
2026-07-18 11:36:01 -0500

    feat: implementar la clase FlatWhite.java que implementa la interfaz Cafe

---
+++ src/FlatWhite.java
@@ -0,0 +1,10 @@
+public class FlatWhite implements Cafe {
+
+    public String getDescripcion() {
+        return "Flat White";
+    }
+
+    public double getCosto() {
+        return 3.00;
+    }
+}
