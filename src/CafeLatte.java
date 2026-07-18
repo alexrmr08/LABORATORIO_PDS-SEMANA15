b8d2e0191102aa926dbfe23601bf3a0ddf135a77
alexrmr08 <walter.romero123.2@gmail.com>
2026-07-18 11:37:28 -0500

    feat: implementar la clase CafeLatte.java que implementa la clase Cafe

---
+++ src/CafeLatte.java
@@ -0,0 +1,10 @@
+public class CafeLatte implements Cafe {
+
+    public String getDescripcion() {
+        return "Cafe Latte";
+    }
+
+    public double getCosto() {
+        return 3.50;
+    }
+}
