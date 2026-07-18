35f251f05fcd27ab2b1d80a79bdc084ca78ab1c6
alexrmr08 <walter.romero123.2@gmail.com>
2026-07-18 11:33:48 -0500

    feat; implementar la clase EspressoMacchiato.java que implementa la interfaz Cafe

---
+++ src/EspressoMacchiato.java
@@ -0,0 +1,10 @@
+public class EspressoMacchiato implements Cafe {
+
+    public String getDescripcion() {
+        return "Espresso Macchiato";
+    }
+
+    public double getCosto() {
+        return 2.50;
+    }
+}
